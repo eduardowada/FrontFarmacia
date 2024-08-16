1d53248b4ed8c188ff703067e1dca176c31e0b9e
eduardowada <156962925+eduardowada@users.noreply.github.com>
2024-08-16 13:55:22 -0300

    add routes

--- FrontFarmacia/src/components/navbar/Navbar.tsx
+++ FrontFarmacia/src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Pill } from '@phosphor-icons/react'
 
 function Navbar() {
@@ -7,16 +8,16 @@ function Navbar() {
       <div className='w-full bg-orange-700 text-white flex justify-center py-4'>
         <div className="container flex justify-between text-lg">
           <div className='flex items-center text-2xl font-bold uppercase'>
-            <Pill size={48} weight='bold' className='mr-2'/>
+            <Pill size={30} weight='bold' className='mr-2'/>
             UltraFarma Generation
           </div>
 
-          <div className='flex gap-4'>
-            <div className='hover:underline'>Categoria</div>
-            <div className='hover:underline'>Produto</div>
-            <div className='hover:underline'>Usuario</div>
-            <div className='hover:underline'>Sair</div>
-          </div>
+          <div className='flex gap-4 mr-2'>
+              <Link to='/home' className='hover:underline'>Home</Link>
+              <div className='hover:underline'>Categorias</div>
+              <div className='hover:underline'>Produtos</div>
+              <div className='hover:underline'>Sair</div>
+            </div>
         </div>
       </div>
     </>
